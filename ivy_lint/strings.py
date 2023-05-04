784dfb0e3eb3fd0e9b5a7021669f59a3603d8581
Kareem Morsy <kreem.morsy@hotmail.com>
2023-05-04 21:46:17 +0000

    Hotfix add debug info

--- ivy_lint/strings.py
+++ ivy_lint/strings.py
@@ -26,4 +26,11 @@ def find_closing_parentheses(string: str, start: int) -> int:
             count -= 1
             if count == 0:
                 return i
-    raise ValueError("No closing parentheses found")
+
+    start_line = string.rfind("\n", 0, start)
+    end_line = string.find("\n", start)
+    if start_line == -1:
+        start_line = 0
+    if end_line == -1:
+        end_line = len(string)
+    raise ValueError(f"No closing parentheses found for {string[start_line:end_line]}")
