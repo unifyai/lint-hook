cf7d1515ffbe9fd511b8440ee7fbbaab5d422e13
NripeshN <nn2012@hw.ac.uk>
2023-08-22 01:32:29 +0530

    helper api fix

--- ivy_lint/formatters/function_ordering.py
+++ ivy_lint/formatters/function_ordering.py
@@ -1,5 +1,3 @@
-"""Formatter for function ordering."""
-
 import re
 import ast
 from typing import Tuple, List
@@ -13,10 +11,6 @@ class FunctionOrderingFormatter(BaseFormatter):
     def _extract_node_with_leading_comments(
         self, node: ast.AST, source_code: str
     ) -> Tuple[str, ast.AST]:
-        """
-        Extract the source code of a node along with its leading comments,
-        decorators, and whitespaces.
-        """
         if hasattr(node, "decorator_list"):
             start_line = (
                 node.decorator_list[0].lineno if node.decorator_list else node.lineno
@@ -25,15 +19,12 @@ class FunctionOrderingFormatter(BaseFormatter):
             start_line = node.lineno
 
         end_line = getattr(node, "end_lineno", node.lineno)
-
         lines = source_code.splitlines()
         extracted_lines = []
 
-        # Get the actual code for the node
         for line_num in range(start_line - 1, end_line):
             extracted_lines.append(lines[line_num])
 
-        # Get the leading comments and whitespaces
         for line_num in range(start_line - 2, -1, -1):
             stripped = lines[line_num].strip()
             if not stripped or stripped.startswith("#"):
@@ -41,38 +32,25 @@ class FunctionOrderingFormatter(BaseFormatter):
             else:
                 break
 
-        # Remove duplicate headers
-        extracted_lines = [line for line in extracted_lines if line not in ["# Helpers #", "# ------- #", "# API Functions #", "# ------------- #"]]
-
         return "\n".join(extracted_lines), node
 
     def _extract_all_nodes_with_comments(
         self, tree: ast.AST, source_code: str
     ) -> List[Tuple[str, ast.AST]]:
-        """
-        Extract all the nodes in the AST with their associated comments and
-        whitespaces.
-        """
         return [
             self._extract_node_with_leading_comments(node, source_code)
             for node in tree.body
         ]
 
     def _rearrange_functions_and_classes(self, source_code: str) -> str:
-        """
-        Rearrange classes and functions in source code alphabetically with
-        comments.
-        """
         tree = ast.parse(source_code)
 
-        # Rearrange methods inside each class in ascending order
         for node in tree.body:
             if isinstance(node, ast.ClassDef):
                 node.body.sort(key=lambda n: n.name if isinstance(n, ast.FunctionDef) else "")
 
         nodes_with_comments = self._extract_all_nodes_with_comments(tree, source_code)
 
-        # Define a helper function to determine sorting precedence
         def sort_key(item):
             node = item[1]
             if isinstance(node, (ast.Import, ast.ImportFrom)):
@@ -89,25 +67,28 @@ class FunctionOrderingFormatter(BaseFormatter):
             return (5, 0, getattr(node, "name", ""))
 
         nodes_sorted = sorted(nodes_with_comments, key=sort_key)
-
         reordered_code_list = []
         prev_was_assignment = False
-        added_helpers_header = False
-        added_api_header = False
+        last_function_type = None
 
         for code, node in nodes_sorted:
-            if isinstance(node, ast.FunctionDef) and node.name.startswith("_") and not added_helpers_header:
-                reordered_code_list.append("# Helpers #")
-                reordered_code_list.append("# ------- #")
-                added_helpers_header = True
-            elif isinstance(node, ast.FunctionDef) and not node.name.startswith("_") and not added_api_header:
-                reordered_code_list.append("# API Functions #")
-                reordered_code_list.append("# ------------- #")
-                added_api_header = True
+            current_function_type = None
+            if isinstance(node, ast.FunctionDef):
+                if node.name.startswith("_"):
+                    current_function_type = "helper"
+                    if last_function_type != "helper":
+                        reordered_code_list.append("# Helpers #")
+                        reordered_code_list.append("# ------- #")
+                else:
+                    current_function_type = "api"
+                    if last_function_type != "api":
+                        reordered_code_list.append("# API Functions #")
+                        reordered_code_list.append("# ------------- #")
+
+            last_function_type = current_function_type or last_function_type
 
             if isinstance(node, ast.Assign):
                 if prev_was_assignment:
-                    # If the previous node was also an assignment, skip one newline
                     reordered_code_list.append(code.strip())
                 else:
                     reordered_code_list.append(code)
@@ -117,29 +98,19 @@ class FunctionOrderingFormatter(BaseFormatter):
                 prev_was_assignment = False
 
         reordered_code = "\n".join(reordered_code_list).strip()
-
-        # Ensure there's a newline at the end of the file
         if not reordered_code.endswith("\n"):
             reordered_code += "\n"
 
         return reordered_code
 
     def _format_file(self, filename: str) -> bool:
-        # Only include ivy frontend files
-        if (
-            re.match(
-                r"ivy/functional/frontends/(?!.*(?:config\.py|__init__\.py)$).*", 
-                filename,
-            )
-            is None
-        ):
+        if re.match(r"ivy/functional/frontends/(?!.*(?:config\.py|__init__\.py)$).*", filename) is None:
             return False
 
         try:
             with open(filename, "r", encoding="utf-8") as f:
                 original_code = f.read()
 
-            # Ignore the file if it's empty or contains only whitespace
             if not original_code.strip():
                 return False
 
@@ -149,8 +120,5 @@ class FunctionOrderingFormatter(BaseFormatter):
                 f.write(reordered_code)
 
         except SyntaxError:
-            print(
-                f"Error: The provided file '{filename}' does not contain valid Python "
-                "code."
-            )
+            print(f"Error: The provided file '{filename}' does not contain valid Python code.")
             return False
