612c0c045a03f0f5a54769968c5c1539d4104524
NripeshN <nn2012@hw.ac.uk>
2023-08-22 15:57:42 +0530

    Clean code

--- ivy_lint/formatters/function_ordering.py
+++ ivy_lint/formatters/function_ordering.py
@@ -1,63 +1,42 @@
 import re
 import ast
 from typing import Tuple, List
-
 from ivy_lint.formatters import BaseFormatter
 
-HEADERS_TO_REMOVE = [
-    "# Helpers #",
-    "# Main #",
-    "# --- Helpers --- #",
-    "# --- Main --- #",
-    " API Functions ",
-    "# - #",
-    "# -- #"
-    "# --- #",
-    "# ---- #",
-    "# ----- #",
-    "# ------ #",
-    "# ------- #",
-    "# -------- #",
-    "# --------- #",
-    "# ---------- #",
-    "# ----------- #",
-    "# ------------ #",
-    "# ------------- #",
-    "# -------------- #",
-    "# --------------- #",
-    "# ---------------- #",
-    "# ----------------- #",
-    "# ------------------ #",
-    "# ------------------- #",
-    "# -------------------- #",
-    "# --------------------- #",
-]
+HEADER_PATTERN = re.compile(r"# ?[-]+ ?(?:Helpers|Main|API Functions)? ?[-]+ ?#")
 
 class FunctionOrderingFormatter(BaseFormatter):
     """Formatter for function ordering."""
 
-    def _remove_existing_headers(self, source_code: str) -> str:
-        for header in HEADERS_TO_REMOVE:
-            source_code = source_code.replace(header, "")
-        return source_code
+    @staticmethod
+    def _remove_existing_headers(source_code: str) -> str:
+        return HEADER_PATTERN.sub("", source_code)
 
-    def _extract_node_with_leading_comments(
-        self, node: ast.AST, source_code: str
-    ) -> Tuple[str, ast.AST]:
+    @staticmethod
+    def _sort_key(item) -> Tuple[int, str]:
+        node = item[1]
+        order = {
+            ast.Import: 0,
+            ast.ImportFrom: 0,
+            ast.Assign: 1,
+            ast.ClassDef: 2,
+            ast.FunctionDef: 3 if node.name.startswith("_") else 4,
+        }
+        node_type = type(node)
+        if node_type in order:
+            return (order[node_type], getattr(node, "name", ""))
+        return (5, "")
+
+    def _extract_node_with_leading_comments(self, node: ast.AST, source_code: str) -> Tuple[str, ast.AST]:
         if hasattr(node, "decorator_list"):
-            start_line = (
-                node.decorator_list[0].lineno if node.decorator_list else node.lineno
-            )
+            start_line = node.decorator_list[0].lineno if node.decorator_list else node.lineno
         else:
             start_line = node.lineno
 
         end_line = getattr(node, "end_lineno", node.lineno)
         lines = source_code.splitlines()
-        extracted_lines = []
-
-        for line_num in range(start_line - 1, end_line):
-            extracted_lines.append(lines[line_num])
-
+        extracted_lines = [lines[line_num] for line_num in range(start_line - 1, end_line)]
+        
         for line_num in range(start_line - 2, -1, -1):
             stripped = lines[line_num].strip()
             if not stripped or stripped.startswith("#"):
@@ -67,81 +46,36 @@ class FunctionOrderingFormatter(BaseFormatter):
 
         return "\n".join(extracted_lines), node
 
-    def _extract_all_nodes_with_comments(
-        self, tree: ast.AST, source_code: str
-    ) -> List[Tuple[str, ast.AST]]:
-        return [
-            self._extract_node_with_leading_comments(node, source_code)
-            for node in tree.body
-        ]
+    def _extract_all_nodes_with_comments(self, tree: ast.AST, source_code: str) -> List[Tuple[str, ast.AST]]:
+        return [self._extract_node_with_leading_comments(node, source_code) for node in tree.body]
 
     def _rearrange_functions_and_classes(self, source_code: str) -> str:
-        source_code = self._remove_existing_headers(source_code)
-        
+        source_code = self._remove_existing_headers(source_code.strip())
+
         tree = ast.parse(source_code)
-        for node in tree.body:
-            if isinstance(node, ast.ClassDef):
-                node.body.sort(key=lambda n: n.name if isinstance(n, ast.FunctionDef) else "")
-
         nodes_with_comments = self._extract_all_nodes_with_comments(tree, source_code)
+        nodes_sorted = sorted(nodes_with_comments, key=self._sort_key)
 
-        def sort_key(item):
-            node = item[1]
-            if isinstance(node, (ast.Import, ast.ImportFrom)):
-                return (0, 0, getattr(node, "name", ""))
-            if isinstance(node, ast.Assign):
-                targets = [t.id for t in node.targets if isinstance(t, ast.Name)]
-                return (1, 0, ",".join(targets))
-            if isinstance(node, ast.ClassDef):
-                return (2, 0, node.name)
-            if isinstance(node, ast.FunctionDef):
-                if node.name.startswith("_"):
-                    return (3, 0, node.name)
-                return (4, 0, node.name)
-            return (5, 0, getattr(node, "name", ""))
-
-        nodes_sorted = sorted(nodes_with_comments, key=sort_key)
-
-        has_helper_functions = any(
-            isinstance(node, ast.FunctionDef) and node.name.startswith("_")
-            for _, node in nodes_sorted
-        )
-
-        reordered_code_list = []
-        prev_was_assignment = False
-        last_function_type = None
+        segments = {
+            "imports": [],
+            "assignments": [],
+            "helpers": ["\n\n# --- Helpers --- #", "# --------------- #"],
+            "apis": ["\n\n# --- Main --- #", "# ------------ #"]
+        }
 
         for code, node in nodes_sorted:
-            current_function_type = None
-            if isinstance(node, ast.FunctionDef):
+            if isinstance(node, (ast.Import, ast.ImportFrom)):
+                segments["imports"].append(code)
+            elif isinstance(node, ast.Assign):
+                segments["assignments"].append(code)
+            elif isinstance(node, ast.FunctionDef):
                 if node.name.startswith("_"):
-                    current_function_type = "helper"
-                    if last_function_type != "helper":
-                        reordered_code_list.append("\n\n# --- Helpers --- #")
-                        reordered_code_list.append("# --------------- #")
+                    segments["helpers"].append(code)
                 else:
-                    current_function_type = "api"
-                    if last_function_type != "api" and has_helper_functions:
-                        reordered_code_list.append("\n\n# --- Main --- #")
-                        reordered_code_list.append("# ------------ #")
+                    segments["apis"].append(code)
 
-            last_function_type = current_function_type or last_function_type
-
-            if isinstance(node, ast.Assign):
-                if prev_was_assignment:
-                    reordered_code_list.append(code.strip())
-                else:
-                    reordered_code_list.append(code)
-                prev_was_assignment = True
-            else:
-                reordered_code_list.append(code)
-                prev_was_assignment = False
-
-        reordered_code = "\n".join(reordered_code_list).strip()
-        if not reordered_code.endswith("\n"):
-            reordered_code += "\n"
-
-        return reordered_code
+        reordered_code = "\n".join(segments["imports"] + segments["assignments"] + segments["helpers"] + segments["apis"])
+        return reordered_code + "\n" if not reordered_code.endswith("\n") else reordered_code
 
     def _format_file(self, filename: str) -> bool:
         if re.match(r"ivy/functional/frontends/(?!.*(?:config\.py|__init__\.py)$).*", filename) is None:
@@ -162,3 +96,5 @@ class FunctionOrderingFormatter(BaseFormatter):
         except SyntaxError:
             print(f"Error: The provided file '{filename}' does not contain valid Python code.")
             return False
+
+        return True  # indicate successful formatting
