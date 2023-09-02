03c447659cc8979e2e3d801d8d8be10ea946ee92
NripeshN <nn2012@hw.ac.uk>
2023-09-03 01:49:45 +0530

    small fix

--- ivy_lint/formatters/class_ordering.py
+++ ivy_lint/formatters/class_ordering.py
@@ -15,7 +15,7 @@ def has_property_decorators(node: ast.FunctionDef) -> bool:
         for decorator in node.decorator_list
     )
 
-def extract_dependencies_from_assignments(class_node: ast.ClassDef):
+def assignment_build_dependency_graph_for_class(class_node: ast.ClassDef):
     graph = nx.DiGraph()
 
     for node in class_node.body:
@@ -31,40 +31,48 @@ def extract_dependencies_from_assignments(class_node: ast.ClassDef):
 
 class ClassFunctionOrderingFormatter:
 
-    def rearrange_functions_and_classes(self, source_code: str) -> str:
+    def rearrange_functions_within_class(self, class_node: ast.ClassDef) -> ast.ClassDef:
+
+        dependency_graph = assignment_build_dependency_graph_for_class(class_node)
+
+        # Split nodes into various categories
+        independent_assignments, dependent_assignments, properties, methods = [], [], [], []
+
+        for inner_node in class_node.body:
+            if isinstance(inner_node, ast.Assign) and isinstance(inner_node.targets[0], ast.Name):
+                if not list(dependency_graph.predecessors(inner_node.targets[0].id)):
+                    independent_assignments.append(inner_node)
+                else:
+                    dependent_assignments.append(inner_node)
+            elif isinstance(inner_node, ast.FunctionDef):
+                if has_property_decorators(inner_node):
+                    properties.append(inner_node)
+                else:
+                    methods.append(inner_node)
+
+        # Construct reordered body
+        class_node.body = [
+            *independent_assignments,
+            ast.Expr(ast.Str(s="# Properties #\n# ---------- #")),
+            *properties,
+            ast.Expr(ast.Str(s="# Instance Methods #\n# ---------------- #")),
+            *methods,
+            *dependent_assignments
+        ]
+        
+        return class_node
+
+    def rearrange_class_in_source(self, source_code: str) -> str:
         tree = ast.parse(source_code)
+        reordered_nodes = []
 
         for node in tree.body:
             if isinstance(node, ast.ClassDef):
+                reordered_nodes.append(self.rearrange_functions_within_class(node))
+            else:
+                reordered_nodes.append(node)
 
-                # Split nodes
-                independent_assignments, dependent_assignments = [], []
-                properties, methods = [], []
-
-                dependency_graph = extract_dependencies_from_assignments(node)
-                for assignment_node in dependency_graph:
-                    if not list(dependency_graph.predecessors(assignment_node)):
-                        independent_assignments.append(assignment_node)
-                    else:
-                        dependent_assignments.append(assignment_node)
-
-                for inner_node in node.body:
-                    if isinstance(inner_node, ast.FunctionDef):
-                        if has_property_decorators(inner_node):
-                            properties.append(inner_node)
-                        else:
-                            methods.append(inner_node)
-
-                # Construct reordered body
-                node.body = [
-                    *independent_assignments,
-                    ast.Expr(ast.Str(s="# Properties #\n# ---------- #")),
-                    *properties,
-                    ast.Expr(ast.Str(s="# Instance Methods #\n# ---------------- #")),
-                    *methods,
-                    *dependent_assignments
-                ]
-
+        tree.body = reordered_nodes
         return ast.unparse(tree)
 
     def format_file(self, filename: str) -> bool:
@@ -78,15 +86,12 @@ class ClassFunctionOrderingFormatter:
             if not original_code.strip():
                 return False
 
-            reordered_code = self.rearrange_functions_and_classes(original_code)
+            reordered_code = self.rearrange_class_in_source(original_code)
 
             with open(filename, "w", encoding="utf-8") as f:
                 f.write(reordered_code)
             return True
 
         except SyntaxError:
-            print(
-                f"Error: The provided file '{filename}' does not contain valid Python code."
-            )
+            print(f"Error: The provided file '{filename}' does not contain valid Python code.")
             return False
-
