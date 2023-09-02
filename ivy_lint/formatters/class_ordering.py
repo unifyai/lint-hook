634e4509ebde404766e490d2ba3ea6060dc27833
NripeshN <nn2012@hw.ac.uk>
2023-09-03 01:34:21 +0530

    test 44

--- ivy_lint/formatters/class_ordering.py
+++ ivy_lint/formatters/class_ordering.py
@@ -1,8 +1,7 @@
-import ast
 import re
+import ast
 import networkx as nx
-import sys
-from typing import List
+from typing import Tuple, List
 
 from ivy_lint.formatters import BaseFormatter
 
@@ -11,91 +10,96 @@ FILE_PATTERN = re.compile(
     r"|ivy_tests/test_ivy/(?!.*(?:__init__\.py|conftest\.py|helpers/.*|test_frontends/config/.*$)).*)"
 )
 
-PROPERTIES_HEADER = "\n# Properties #\n# ---------- #\n"
-METHODS_HEADER = "\n# Instance Methods #\n# ---------------- #\n"
 
-def extract_names_from_assignment(node: ast.Assign) -> List[str]:
+def extract_names_from_assignment_inside_class(node: ast.Assign) -> List[str]:
     names = []
 
-    def extract_names(node):
+    def extract_names_from_node(node):
         if isinstance(node, ast.Name):
             names.append(node.id)
         for child in ast.iter_child_nodes(node):
-            extract_names(child)
+            extract_names_from_node(child)
 
-    # Handle Python version differences
-    if sys.version_info < (3, 8):
-        values = node.values
-    else:
-        values = [node.value]
-
-    for value in values:
-        extract_names(value)
-
+    extract_names_from_node(node.value)
     return names
+
+
+def assignment_build_dependency_graph_for_class(class_node: ast.ClassDef):
+    graph = nx.DiGraph()
+
+    for node in class_node.body:
+        if isinstance(node, ast.Assign):
+            for target in node.targets:
+                if isinstance(target, ast.Name):
+                    graph.add_node(target.id)
+
+    for node in class_node.body:
+        if isinstance(node, ast.Assign):
+            right_side_names = extract_names_from_assignment_inside_class(node)
+            for target in node.targets:
+                if isinstance(target, ast.Name):
+                    for name in right_side_names:
+                        if graph.has_node(name):
+                            graph.add_edge(name, target.id)
+
+    return graph
+
+
+def has_property_decorators(node: ast.FunctionDef) -> bool:
+    return any(
+        isinstance(decorator, ast.Attribute) and (
+            decorator.attr.endswith(".setter") or decorator.attr.endswith(".getter") or decorator.attr == "property"
+        )
+        for decorator in node.decorator_list
+    )
+
 
 class ClassFunctionOrderingFormatter(BaseFormatter):
 
-    def _build_intra_class_dependency_graph(self, class_body):
-        graph = nx.DiGraph()
-        
-        # Add nodes
-        for node in class_body:
-            if isinstance(node, ast.FunctionDef):
-                graph.add_node(node.name)
-            elif isinstance(node, ast.Assign):
-                for target in node.targets:
-                    if isinstance(target, ast.Name):
-                        graph.add_node(target.id)
+    def _extract_all_nodes_within_class(self, class_node: ast.ClassDef) -> List[ast.AST]:
+        return class_node.body
 
-        # Add edges
-        for node in class_body:
-            if isinstance(node, ast.Assign):
-                right_side_names = extract_names_from_assignment(node)
-                for target in node.targets:
-                    if isinstance(target, ast.Name):
-                        for name in right_side_names:
-                            if graph.has_node(name):
-                                graph.add_edge(name, target.id)
+    def _rearrange_functions_and_classes(self, source_code: str) -> str:
+        tree = ast.parse(source_code)
 
-        return graph
+        for node in tree.body:
+            if isinstance(node, ast.ClassDef):
+                nodes_within_class = self._extract_all_nodes_within_class(node)
 
-    def _rearrange_class_content(self, class_node: ast.ClassDef) -> List[ast.AST]:
-        graph = self._build_intra_class_dependency_graph(class_node.body)
-        
-        dependent_assignments = set(graph.nodes()) - set(graph.edges())
-        non_dependent_assignments = [
-            node for node in class_node.body if isinstance(node, ast.Assign) and all(
-                isinstance(target, ast.Name) and target.id not in dependent_assignments
-                for target in node.targets
-            )
-        ]
+                # Dependency graph for assignments inside class
+                assignment_dependency_graph_for_class = assignment_build_dependency_graph_for_class(node)
+                dependent_assignments = set(assignment_dependency_graph_for_class.nodes()) - set(
+                    assignment_dependency_graph_for_class.edges()
+                )
+                independent_assignments = set(assignment_dependency_graph_for_class.nodes()) - dependent_assignments
 
-        properties = [
-            node for node in class_node.body if any(
-                isinstance(decorator, ast.Attribute) and decorator.attr in ["setter", "getter", "property"]
-                for decorator in node.decorator_list
-            )
-        ]
-        properties = sorted(properties, key=lambda x: x.name)
+                reordered_nodes = []
 
-        other_methods = [
-            node for node in class_node.body if isinstance(node, ast.FunctionDef)
-            and node not in properties
-        ]
-        other_methods = sorted(other_methods, key=lambda x: x.name)
+                # 1. Add independent assignments
+                for inner_node in nodes_within_class:
+                    if isinstance(inner_node, ast.Assign) and inner_node.targets[0].id in independent_assignments:
+                        reordered_nodes.append(inner_node)
 
-        # Reconstruct class content
-        new_body = non_dependent_assignments
-        if properties:
-            new_body.extend([ast.parse(PROPERTIES_HEADER).body[0]])
-            new_body.extend(properties)
-        if other_methods:
-            new_body.extend([ast.parse(METHODS_HEADER).body[0]])
-            new_body.extend(other_methods)
-        new_body.extend([node for node in class_node.body if node not in (non_dependent_assignments + properties + other_methods)])
-        
-        return new_body
+                # 2. Add property-related functions
+                reordered_nodes.append("# Properties #\n# ---------- #")
+                for inner_node in nodes_within_class:
+                    if isinstance(inner_node, ast.FunctionDef) and has_property_decorators(inner_node):
+                        reordered_nodes.append(inner_node)
+
+                # 3. Add other functions
+                reordered_nodes.append("# Instance Methods #\n# ---------------- #")
+                for inner_node in nodes_within_class:
+                    if isinstance(inner_node, ast.FunctionDef) and not has_property_decorators(inner_node):
+                        reordered_nodes.append(inner_node)
+
+                # 4. Add dependent assignments
+                for inner_node in nodes_within_class:
+                    if isinstance(inner_node, ast.Assign) and inner_node.targets[0].id in dependent_assignments:
+                        reordered_nodes.append(inner_node)
+
+                node.body = reordered_nodes
+
+        return ast.unparse(tree)
 
     def _format_file(self, filename: str) -> bool:
         if FILE_PATTERN.match(filename) is None:
@@ -105,15 +109,13 @@ class ClassFunctionOrderingFormatter(BaseFormatter):
             with open(filename, "r", encoding="utf-8") as f:
                 original_code = f.read()
 
-            tree = ast.parse(original_code)
-            for node in tree.body:
-                if isinstance(node, ast.ClassDef):
-                    node.body = self._rearrange_class_content(node)
+            if not original_code.strip():
+                return False
 
-            new_code = ast.unparse(tree)
+            reordered_code = self._rearrange_functions_and_classes(original_code)
 
             with open(filename, "w", encoding="utf-8") as f:
-                f.write(new_code)
+                f.write(reordered_code)
 
         except SyntaxError:
             print(
@@ -121,5 +123,3 @@ class ClassFunctionOrderingFormatter(BaseFormatter):
                 " code."
             )
             return False
-
-        return True
