bb185b0c384c906fd4c275ba5aa1302c33db0876
NripeshN <nn2012@hw.ac.uk>
2023-09-06 00:58:55 +0530

    big changes

--- ivy_lint/formatters/docs_formatter.py
+++ ivy_lint/formatters/docs_formatter.py
@@ -1,55 +1,55 @@
 import re
 from ivy_lint.formatters import BaseFormatter
 
-EXAMPLE_HEADER_PATTERN = re.compile(r'Functional Examples\n-{2,}')
-BACKTICKS_PATTERN = re.compile(r'```')
-EXAMPLES_SPACING_PATTERN = re.compile(r'(Examples\n-{2,}\n)([^`]*?)(\n\n|\Z)', re.DOTALL)
 
-class DocsFormatter(BaseFormatter):
+class DocstringFormatter(BaseFormatter):
+    """Formatter for correcting docstrings following ivy style."""
     
-    def _format_examples_header(self, source_code: str) -> str:
-        return EXAMPLE_HEADER_PATTERN.sub('Examples\n--------', source_code)
-
-    def _format_examples_spacing(self, source_code: str) -> str:
-        def fix_spacing(match):
-            example = match.group(2)
-            example = re.sub(r'\n{3,}', '\n\n', example)
-            example = re.sub(r'\n(?=[^ \n])', '\n\n', example)
-            example = re.sub(r'(?<=[^ \n])\n', '\n\n', example)
-            return 'Examples\n--------\n' + example + match.group(3)
-
-        return EXAMPLES_SPACING_PATTERN.sub(fix_spacing, source_code)
-
-    def _format_remove_backticks(self, source_code: str) -> str:
-        def remove_ticks(match):
-            example = match.group(2)
-            example = BACKTICKS_PATTERN.sub('', example)
-            return 'Examples\n--------\n' + example + match.group(3)
-
-        return EXAMPLES_SPACING_PATTERN.sub(remove_ticks, source_code)
+    # Patterns to identify and correct issues
+    INCORRECT_EXAMPLES_PATTERN = re.compile(r'Functional Examples(?=:)', re.IGNORECASE)
+    EXAMPLES_BLANK_SPACE_PATTERN = re.compile(r'(?<=Examples:)\n*?(>>>[^\n]*?\n[^\n]*?)\n*?(?=\w|$)', re.DOTALL)
+    BACKTICKS_PATTERN = re.compile(r'(?<=Examples:.*?)(```\s*?>>>.*?```)', re.DOTALL)
+    
+    def _fix_examples_title(self, content: str) -> str:
+        """
+        Ensure that the examples section is correctly titled "Examples".
+        """
+        return self.INCORRECT_EXAMPLES_PATTERN.sub("Examples", content)
+    
+    def _fix_examples_format_for_sphinx(self, content: str) -> str:
+        """
+        Ensure there's a blank line above and below examples.
+        Also ensures there's only one blank line between the example input and output.
+        """
+        def repl(match):
+            example = match.group(1).strip()
+            example = re.sub(r'\n+', '\n', example)  # Remove multiple newlines
+            return f'\n\n{example}\n'
         
+        return self.EXAMPLES_BLANK_SPACE_PATTERN.sub(repl, content)
+
+    def _remove_backticks_from_examples(self, content: str) -> str:
+        """
+        Remove backticks around examples.
+        """
+        def repl(match):
+            return match.group(1).replace('```', '').strip()
+        
+        return self.BACKTICKS_PATTERN.sub(repl, content)
+
     def _format_file(self, filename: str) -> bool:
-        try:
-            with open(filename, 'r', encoding="utf-8") as f:
-                source_code = f.read()
+        """Format the given file and return True if it was changed."""
+        with open(filename, 'r', encoding='utf-8') as f:
+            original_content = f.read()
 
-            if not source_code.strip():
-                return False
+        formatted_content = self._fix_examples_title(original_content)
+        formatted_content = self._fix_examples_format_for_sphinx(formatted_content)
+        formatted_content = self._remove_backticks_from_examples(formatted_content)
 
-            source_code = self._format_examples_header(source_code)
-            source_code = self._format_examples_spacing(source_code)
-            source_code = self._format_remove_backticks(source_code)
+        # Check if the file was changed
+        if formatted_content != original_content:
+            with open(filename, 'w', encoding='utf-8') as f:
+                f.write(formatted_content)
+            return True
 
-            with open(filename, 'w', encoding="utf-8") as f:
-                f.write(source_code)
-
-        except Exception as e:
-            print(f"Error while formatting '{filename}': {e}")
-            return False
-        return True
-
-    def format(self) -> bool:
-        changed = False
-        for filename in self.filenames:
-            changed = self._format_file(filename) or changed
-        return changed
+        return False
