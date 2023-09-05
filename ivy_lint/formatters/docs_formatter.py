05c73cd72c65e53132558d5df38b27f1871137c7
NripeshN <nn2012@hw.ac.uk>
2023-09-05 23:57:06 +0530

    extract docstrings fix

--- ivy_lint/formatters/docs_formatter.py
+++ ivy_lint/formatters/docs_formatter.py
@@ -8,77 +8,81 @@ EXAMPLES_PATTERN = re.compile(
 )
 
 class DocsFormatter(BaseFormatter):
+    """Formatter for docstrings."""
 
     @staticmethod
     def correct_docstring(docstring: str) -> str:
-        """Apply corrections to the given docstring."""
-
-        # Replace 'Functional Examples' with 'Examples' and adjust the dashes
         docstring = re.sub(r'Functional Examples\n-{3,}', 'Examples\n--------', docstring)
-
-        # Correcting the entire Examples section
-        def fix_examples_section(match):
-            examples_section = match.group(2)
-            
-            # Split the section into lines
-            lines = examples_section.split('\n')
-            in_code_block = False
-            new_lines = []
-
-            for line in lines:
-                # Detect the start or continuation of a code block
-                if line.strip().startswith(">>>"):
-                    if not in_code_block:
-                        new_lines.append("")  # Add blank line before starting a new block
-                        in_code_block = True
-                # Detect the end of a code block
-                elif in_code_block and not line.strip():
-                    in_code_block = False
-                    new_lines.append(line)  # Keep the existing blank line
-                    continue
-
-                new_lines.append(line)
-
-            # If it ends while still in a code block, ensure it finishes with a blank line
-            if in_code_block:
-                new_lines.append("")
-
-            return match.group(1) + '\n'.join(new_lines) + "\n"
-
-        # Adjust the examples section
-        docstring = EXAMPLES_PATTERN.sub(fix_examples_section, docstring)
-
+        docstring = EXAMPLES_PATTERN.sub(DocsFormatter._fix_examples_section, docstring)
         return docstring
 
-    def _extract_docstrings_from_tree(self, tree: ast.AST) -> List[ast.Str]:
-        """Extract all docstrings from an AST tree."""
-        return [node.value for node in ast.walk(tree) if isinstance(node, ast.Expr) and isinstance(node.value, ast.Str)]
+    @staticmethod
+    def _fix_examples_section(match):
+        """Reformat the Examples section of a docstring."""
+        examples_section = match.group(2)
+        lines = examples_section.split('\n')
+        
+        in_code_block = False
+        new_lines = []
 
-    def _correct_docstrings(self, source_code: str) -> str:
-        """Correct docstrings in the provided source code."""
+        for line in lines:
+            if line.strip().startswith(">>>"):
+                if not in_code_block:
+                    new_lines.append("")
+                    in_code_block = True
+            elif in_code_block and not line.strip():
+                in_code_block = False
+                new_lines.append(line)
+                continue
+
+            new_lines.append(line)
+
+        if in_code_block:
+            new_lines.append("")
+
+        return match.group(1) + '\n'.join(new_lines) + "\n"
+
+    def _extract_docstrings(self, tree: ast.AST) -> List[str]:
+        """Extract all docstrings from an AST tree."""
+        docstrings = []
+
+        for node in ast.walk(tree):
+            # Check if the node is one of the constructs that can have a docstring
+            if isinstance(node, (ast.FunctionDef, ast.AsyncFunctionDef, ast.ClassDef, ast.Module)):
+                docstring = ast.get_docstring(node)
+                if docstring:
+                    docstrings.append(docstring)
+        
+        return docstrings
+
+
+    def _replace_docstrings(self, source_code: str) -> str:
+        """Replace docstrings in the provided source code with corrected versions."""
         tree = ast.parse(source_code)
-        docstrings = self._extract_docstrings_from_tree(tree)
-        
+        docstrings = self._extract_docstrings(tree)
+
+        offset_adjustment = 0
         for doc in docstrings:
-            print(doc.s)
             corrected = self.correct_docstring(doc.s)
-            source_code = source_code.replace(doc.s, corrected)
-        
+
+            if corrected != doc.s:
+                start = doc.col_offset + offset_adjustment
+                end = start + len(doc.s)
+                source_code = source_code[:start] + corrected + source_code[end:]
+                offset_adjustment += len(corrected) - len(doc.s)
+                
         return source_code
 
-    def _format_file(self, filename: str) -> bool:
+    def format_file(self, filename: str) -> bool:
         """Format the file by correcting its docstrings."""
-
         with open(filename, 'r', encoding='utf-8') as f:
             original_code = f.read()
 
-        corrected_code = self._correct_docstrings(original_code)
+        corrected_code = self._replace_docstrings(original_code)
 
-        # Check if any changes have been made.
-        if corrected_code == original_code:
-            return False
+        if corrected_code != original_code:
+            with open(filename, 'w', encoding='utf-8') as f:
+                f.write(corrected_code)
+            return True
 
-        with open(filename, 'w', encoding='utf-8') as f:
-            f.write(corrected_code)
-
-        return True
+        return False
