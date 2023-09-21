c6a5da33aae1731459e39f864ba3c5c191097ffe
Kareem Morsy <kreem.morsy@hotmail.com>
2023-09-21 11:45:21 +0000

    fix: compatibility fix with `isort`

--- ivy_lint/formatters/function_ordering.py
+++ ivy_lint/formatters/function_ordering.py
@@ -275,7 +275,7 @@ class FunctionOrderingFormatter(BaseFormatter):
                     current_function_type = "helper"
                     if last_function_type != "helper":
                         reordered_code_list.append(
-                            "\n\n# --- Helpers --- #\n# --------------- #"
+                            "\n# --- Helpers --- #\n# --------------- #\n"
                         )
                 else:
                     current_function_type = "api"
