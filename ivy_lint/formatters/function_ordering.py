af72c82e651dd285a14bd8292e9f8662f726ff58
NripeshN <nripesh14@gmail.com>
2023-10-10 08:42:34 +0400

    Update function_ordering.py

--- ivy_lint/formatters/function_ordering.py
+++ ivy_lint/formatters/function_ordering.py
@@ -277,7 +277,7 @@ class FunctionOrderingFormatter(BaseFormatter):
                     current_function_type = "helper"
                     if last_function_type != "helper":
                         reordered_code_list.append(
-                            "\n# --- Helpers --- #\n# --------------- #\n"
+                            "# --- Helpers --- #\n# --------------- #"
                         )
                 else:
                     current_function_type = "api"
