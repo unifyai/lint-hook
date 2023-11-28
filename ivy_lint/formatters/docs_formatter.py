532f1c8e80decc720bbd4f7c96568bfb677a0cc9
Bhushan Srivastava <59949692+he11owthere@users.noreply.github.com>
2023-11-29 04:43:58 +0530

    Update docs_formatter.py

--- ivy_lint/formatters/docs_formatter.py
+++ ivy_lint/formatters/docs_formatter.py
@@ -26,7 +26,7 @@ class DocstringFormatter(BaseDocstringFormatter):
             if not is_codeblock and stripped_line.startswith('>>>'):
                 is_codeblock = True
                 codeblock_start_lines.add(idx)
-            elif stripped_line.startswith(('>>>', '...', '[', '(')) or stripped_line.endswith((')', ',', '\\', '(', '[', ']')):
+            elif stripped_line.startswith(('[', '(')) or stripped_line.endswith((')', ',', '\\', '(', '[', ']')):
                 lines_to_modify.add(idx)
             elif is_codeblock and (not stripped_line or (not stripped_line.startswith(('>>>', '...', '[', '(')) and not stripped_line.endswith((')', ',', '\\', '(', '[', ']')))):
                 is_codeblock = False
